67e766e05d0858c9a803e5c9eace369abc5df65a
Azuriru <kegumine@gmail.com>
2024-06-02 17:43:17 +0800

    update utils

--- src/util/types.ts
+++ src/util/types.ts
@@ -3,3 +3,5 @@ export type ValueOf<T> = T[keyof T];
 export type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
 
 export type Booleanish = boolean | 'true' | 'false';
+
+export type ObjectProperty = number | string; // no sane person uses Symbol, ok.
